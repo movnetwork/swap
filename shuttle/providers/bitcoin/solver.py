806f44816a8b2755a17c3b7cae328c9443a7c9be
meherett <meherett@zoho.com>
2019-12-28 06:03:46 +0300

    Renamed solve to solver package

---
+++ shuttle/providers/bitcoin/solver.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+class Solver:
+
+    def __init__(self):
+        pass
+
+
+class FundSolver(Solver):
+
+    def __init__(self, htlc_script):
+        super().__init__()
+
+
+class ClaimSolver(Solver):
+
+    def __init__(self, htlc_script):
+        super().__init__()
+
+
+class RefundSolver(Solver):
+
+    def __init__(self, htlc_script):
+        super().__init__()
+
