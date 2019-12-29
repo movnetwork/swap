3f6ea38ef4fa6bc5b2e86162d4249e888742815c
meherett <meherett@zoho.com>
2019-12-29 10:54:22 +0300

    added Fund, Claim and Refund solver.

--- shuttle/providers/bitcoin/solver.py
+++ shuttle/providers/bitcoin/solver.py
@@ -1,26 +1,62 @@
 #!/usr/bin/env python3
+from btcpy.structs.crypto import PrivateKey
+from btcpy.structs.sig import P2pkhSolver, P2shSolver, IfElseSolver, HashlockSolver, Branch, RelativeTimelockSolver
+from btcpy.structs.transaction import Sequence
+
+from .htlc import HTLC
 
 
-class Solver:
+class FundSolver:
 
-    def __init__(self):
-        pass
+    def __init__(self, private_key):
+        self.private_key = PrivateKey.unhexlify(private_key)
 
-
-class FundSolver(Solver):
-
-    def __init__(self, htlc_script):
-        super().__init__()
+    def solve(self):
+        return P2pkhSolver(self.private_key,
+                           compressed=False)
 
 
-class ClaimSolver(Solver):
+class ClaimSolver:
 
-    def __init__(self, htlc_script):
-        super().__init__()
+    def __init__(self, htlc_script, secret, private_key):
+        if not isinstance(htlc_script, HTLC):
+            raise Exception("HTLC Error!")
+        self.htlc_script = htlc_script.htlc_script
+        self.secret = secret.encode()
+        self.private_key = PrivateKey.unhexlify(private_key)
+
+    def solve(self):
+        return P2shSolver(
+            self.htlc_script,
+            IfElseSolver(
+                Branch.IF,
+                HashlockSolver(
+                    self.secret,
+                    P2pkhSolver(self.private_key,
+                                compressed=False)
+                )
+            )
+        )
 
 
-class RefundSolver(Solver):
+class RefundSolver:
 
-    def __init__(self, htlc_script):
-        super().__init__()
+    def __init__(self, htlc_script, sequence, private_key):
+        if not isinstance(htlc_script, HTLC):
+            raise Exception("HTLC Error!")
+        self.htlc_script = htlc_script.htlc_script
+        self.sequence = sequence
+        self.private_key = PrivateKey.unhexlify(private_key)
 
+    def solve(self):
+        return P2shSolver(
+            self.htlc_script,
+            IfElseSolver(
+                Branch.ELSE,
+                RelativeTimelockSolver(
+                    Sequence(self.sequence),
+                    P2pkhSolver(self.private_key,
+                                compressed=False)
+                )
+            )
+        )
