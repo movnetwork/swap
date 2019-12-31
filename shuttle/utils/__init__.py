4e185eabfa441692908c039a0e9a22def832e410
meherett <meherett@zoho.com>
2019-12-31 05:25:23 +0300

    Update shuttle utils package

--- shuttle/utils/__init__.py
+++ shuttle/utils/__init__.py
@@ -1,8 +1,11 @@
-from .uitls import generate_passphrase, generate_mnemonic
+from .uitls import generate_passphrase,\
+    generate_mnemonic, sha256, double_sha256
 
 
 __all__ = [
     "generate_passphrase",
+    "sha256",
+    "double_sha256",
     "generate_mnemonic"
 ]
 
