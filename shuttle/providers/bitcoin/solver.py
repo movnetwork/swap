898fe72f0a1ca713e5201257896f9562498a9b9a
meherett <meherett@zoho.com>
2019-12-31 05:00:45 +0300

    Upgrade solver.py

--- shuttle/providers/bitcoin/solver.py
+++ shuttle/providers/bitcoin/solver.py
@@ -1,62 +1,70 @@
 #!/usr/bin/env python3
+
 from btcpy.structs.crypto import PrivateKey
-from btcpy.structs.sig import P2pkhSolver, P2shSolver, IfElseSolver, HashlockSolver, Branch, RelativeTimelockSolver
+from btcpy.structs.sig import P2pkhSolver, \
+    IfElseSolver, HashlockSolver, Branch, RelativeTimelockSolver
 from btcpy.structs.transaction import Sequence
 
-from .htlc import HTLC
 
-
+# Fund HTLC Solver
 class FundSolver:
 
-    def __init__(self, private_key):
+    # Initialization funding on hash time lock contract (HTLC)
+    def __init__(self, private_key, compressed=False):
+        # Public key compression
+        self.compressed = compressed
+        # Private key of sender to sign signature
         self.private_key = PrivateKey.unhexlify(private_key)
 
+    # Signature solve
     def solve(self):
-        return P2pkhSolver(self.private_key,
-                           compressed=False)
+        return P2pkhSolver(self.private_key, compressed=self.compressed)
 
 
+# Claim HTLC Solver
 class ClaimSolver:
 
-    def __init__(self, htlc_script, secret, private_key):
-        if not isinstance(htlc_script, HTLC):
-            raise Exception("HTLC Error!")
-        self.htlc_script = htlc_script.htlc_script
+    # Initialization claiming on hash time lock contract (HTLC)
+    def __init__(self, secret, private_key, sequence=5, compressed=False):
+        # Public key compression
+        self.compressed = compressed
+        # Secret key to unlock HTLC
         self.secret = secret.encode()
+        # Private key of recipient to sign signature
         self.private_key = PrivateKey.unhexlify(private_key)
+        # Sequence number of expiration block
+        self.sequence = sequence
 
+    # Signature solve
     def solve(self):
-        return P2shSolver(
-            self.htlc_script,
-            IfElseSolver(
-                Branch.IF,
-                HashlockSolver(
-                    self.secret,
-                    P2pkhSolver(self.private_key,
-                                compressed=False)
-                )
+        return IfElseSolver(
+            Branch.IF,
+            HashlockSolver(
+                self.secret,
+                P2pkhSolver(self.private_key, compressed=self.compressed)
             )
         )
 
 
+# Refund HTLC Solver
 class RefundSolver:
 
-    def __init__(self, htlc_script, sequence, private_key):
-        if not isinstance(htlc_script, HTLC):
-            raise Exception("HTLC Error!")
-        self.htlc_script = htlc_script.htlc_script
-        self.sequence = sequence
+    # Initialization refunding on hash time lock contract (HTLC)
+    def __init__(self, secret, private_key, sequence=5, compressed=False):
+        # Public key compression
+        self.compressed = compressed
+        # Secret key to unlock HTLC
+        self.secret = secret.encode()
+        # Private key of recipient to sign signature
         self.private_key = PrivateKey.unhexlify(private_key)
+        # Sequence number of expiration block
+        self.sequence = sequence
 
     def solve(self):
-        return P2shSolver(
-            self.htlc_script,
-            IfElseSolver(
-                Branch.ELSE,
-                RelativeTimelockSolver(
-                    Sequence(self.sequence),
-                    P2pkhSolver(self.private_key,
-                                compressed=False)
-                )
+        return IfElseSolver(
+            Branch.ELSE,
+            RelativeTimelockSolver(
+                Sequence(self.sequence),
+                P2pkhSolver(self.private_key, compressed=self.compressed)
             )
         )
