4009ae4d3bf7454e8bc009f13df9fa86d14d0f6d
meherett <meherett@zoho.com>
2020-01-03 05:03:15 +0300

    added account_create and list_address functions

--- shuttle/providers/bytom/rpc.py
+++ shuttle/providers/bytom/rpc.py
@@ -15,7 +15,7 @@ bytom = bytom()
 
 
 # Get balance by address
-def get_balance(address, network="testnet", limit=1, page=1, timeout=5):
+def get_balance(address, network="testnet", limit=1, page=1, timeout=bytom["timeout"]):
     parameter = dict(limit=limit, page=page)
     url = str(bytom[network]["blockmeta"]) + ("/address/%s" % address)
     response = requests.get(url=url, params=parameter,
@@ -23,3 +23,25 @@ def get_balance(address, network="testnet", limit=1, page=1, timeout=5):
     if response.status_code == 204:
         return 0
     return response.json()["balance"]
+
+
+# Create account in blockcenter
+def account_create(xpublic_key, label="1st address", email=None,
+                   network="testnet", timeout=bytom["timeout"]):
+    url = str(bytom[network]["blockcenter"]) + "/account/create"
+    data = dict(pubkey=xpublic_key, label=label, email=email)
+    response = requests.post(url=url, data=json.dumps(data),
+                             headers=headers, timeout=timeout)
+    if response.status_code == 200 and response.json()["code"] == 300:
+        raise Exception(response.json()["message"])
+    return response.json()["result"]["data"]
+
+
+# List addresses from blockcenter
+def list_address(guid, limit=10, network="testnet", timeout=bytom["timeout"]):
+    url = str(bytom[network]["blockcenter"]) + "/account/list-address"
+    response = requests.post(url=url, data=json.dumps(dict(guid=guid)),
+                             params=dict(limit=limit), headers=headers, timeout=timeout)
+    if response.status_code == 200 and response.json()["code"] == 300:
+        raise Exception(response.json()["message"])
+    return response.json()["result"]["data"]
