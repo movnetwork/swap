552847aa9d891305b873ac61bbf598cedc0d7bfc
meherett <meherett@zoho.com>
2020-01-07 11:32:43 +0300

    Update bytom utils

--- shuttle/providers/bytom/utils.py
+++ shuttle/providers/bytom/utils.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
 
+import ed25519
+
+
+def sign(private_key_str, message_str):
+    signing_key = ed25519.SigningKey(bytes.fromhex(private_key_str))
+    # signature = signing_key.sign(message_str.encode(), encoding='hex')
+    signature = signing_key.sign(bytes.fromhex(message_str), encoding='hex')
+    return signature.decode()
+
+
+def verify(public_key_str, signature_str, message_str):
+    result = False
+    verifying_key = ed25519.VerifyingKey(public_key_str.encode(), encoding='hex')
+    try:
+        verifying_key.verify(signature_str.encode(), bytes.fromhex(message_str), encoding='hex')
+        result = True
+    except ed25519.BadSignatureError:
+        result = False
+    return result
+
 
 def spend_utxo_action(utxo):
     return dict(type=str("spend_utxo"), output_id=utxo)
