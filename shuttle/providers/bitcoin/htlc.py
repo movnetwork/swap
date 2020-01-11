e2b61e775574a3d91865a212cbfaec877211cf42
meherett <meherett@zoho.com>
2020-01-11 11:54:06 +0300

    Update bitcoin htlc init

--- shuttle/providers/bitcoin/htlc.py
+++ shuttle/providers/bitcoin/htlc.py
@@ -4,7 +4,8 @@ from btcpy.structs.script import Script, ScriptBuilder, P2shScript, \
     IfElseScript, Hashlock256Script, RelativeTimelockScript
 from btcpy.structs.transaction import Sequence
 
-from .utils import script_from_address
+from .utils import script_from_address, is_address
+from ...utils.exceptions import AddressError
 from ...utils import sha256
 
 
@@ -15,13 +16,20 @@ class HTLC:
     def __init__(self, network="testnet"):
         # Bitcoin network.
         self.network = network
-        # Bitcoin network boolean..
-        self.testnet = True if network == "testnet" else False
         # HTLC script
         self.script = None
 
     # Initialize new HTLC Contract script
     def init(self, secret_hash, recipient_address, sender_address, sequence):
+        # Checking parameters
+        if not isinstance(secret_hash, bytes):
+            raise TypeError("Secret hash must be bytes format.")
+        if not is_address(recipient_address, self.network):
+            raise AddressError("Invalid %s recipient %s address." % (self.network, recipient_address))
+        if not is_address(sender_address, self.network):
+            raise AddressError("Invalid %s sender %s address." % (self.network, sender_address))
+        if not isinstance(sequence, int):
+            raise TypeError("Sequence must be integer format.")
         # HASH TIME LOCK CONTRACT SCRIPT
         self.script = IfElseScript(
             # If branch
@@ -74,4 +82,5 @@ class HTLC:
     def address(self):
         if self.script is None:
             raise ValueError("HTLC script is none, Please initialization htlc first.")
-        return P2shScript(self.script.p2sh_hash()).address(mainnet=(not self.testnet))
+        mainnet = True if self.network == "mainnet" else False
+        return P2shScript(self.script.p2sh_hash()).address(mainnet=mainnet)
