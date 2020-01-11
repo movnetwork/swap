9bf9a0566737e8ab1242b19c1c22331e6c8fce18
meherett <meherett@zoho.com>
2020-01-11 11:56:31 +0300

    Update bitcoin utils

--- shuttle/providers/bitcoin/utils.py
+++ shuttle/providers/bitcoin/utils.py
@@ -7,21 +7,26 @@ from btcpy.structs.address import Address
 import cryptos
 import hashlib
 
+from ...utils.exceptions import AddressError, NetworkError
+
 
 # Checking address
-def is_address(address, testnet=True):
+def is_address(address, network="testnet"):
     if isinstance(address, str):
-        if isinstance(testnet, bool):
-            return cryptos.Bitcoin(testnet=testnet).is_address(address)
-        elif isinstance(testnet, str):
-            testnet = True if testnet == "testnet" else False
-            return cryptos.Bitcoin(testnet=testnet).is_address(address)
+        if network == "testnet":
+            return cryptos.Bitcoin(testnet=True).is_address(address)
+        elif network == "mainnet":
+            return cryptos.Bitcoin(testnet=False).is_address(address)
+        else:
+            raise NetworkError("Invalid %s network" % network, "Only testnet or mainnet.")
     raise TypeError("Address must be string format!")
 
 
 # Double SHA256 hash
 def double_sha256(data):
-    return hashlib.sha256(hashlib.sha256(data).digest()).digest()
+    if isinstance(data, bytes):
+        return hashlib.sha256(hashlib.sha256(data).digest()).digest()
+    raise TypeError("Data must be bytes format!")
 
 
 # Transaction fee calculator
@@ -33,18 +38,19 @@ def fee_calculator(transaction_input=1, transaction_output=1):
 
 
 # Setting expiration to script
-def expiration_to_script(expiration):
-    if isinstance(expiration, int):
-        if expiration <= 16:
-            return "OP_%d" % expiration
+def expiration_to_script(sequence):
+    if isinstance(sequence, int):
+        if sequence <= 16:
+            return "OP_%d" % sequence
         else:
-            return Sequence(expiration).for_script()
-    raise TypeError("Expiration must be integer format!")
+            return Sequence(sequence).for_script()
+    raise TypeError("Sequence must be integer format!")
 
 
 # Creating script from address
-def script_from_address(address, testnet=True):
-    assert is_address(address, testnet), "Invalid %s address!"
+def script_from_address(address, network="testnet"):
+    if not is_address(address, network):
+        raise AddressError("Invalid %s %s address!" % (network, address))
     load_address = Address.from_string(address)
     get_type = load_address.get_type()
     if str(get_type) == "p2pkh":
@@ -54,10 +60,7 @@ def script_from_address(address, testnet=True):
 
 
 # Creating script from address
-def address_to_hash(address, testnet=True):
-    if isinstance(address, str) and \
-            cryptos.Bitcoin(testnet=testnet).is_address(address):
-        return Address.from_string(address).hash.hex()
-
-    raise TypeError("Address must be string format!")
-
+def address_to_hash(address, network="testnet"):
+    if not is_address(address, network):
+        raise AddressError("Invalid %s %s address!" % (network, address))
+    return Address.from_string(address).hash.hex()
