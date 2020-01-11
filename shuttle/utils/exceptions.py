6963efb0017018a61393592e830c782da0f4f552
meherett <meherett@zoho.com>
2020-01-11 10:25:30 +0300

    Update exception.py

---
+++ shuttle/utils/exceptions.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+class APIError(Exception):
+
+    def __init__(self, error_message, error_detail):
+        self.error_message = error_message
+        self.error_detail = error_detail
+
+    def __str__(self):
+        return "%s, %s" % (self.error_message, self.error_detail)
+
+
+class ClientError(Exception):
+    def __init__(self, error_message, status_code=None):
+        self.status_code = status_code
+        self.error_message = error_message
+
+    def __str__(self):
+        if self.status_code:
+            return "(%s) %s" % (self.status_code, self.error_message)
+        else:
+            return self.error_message
+
+
+class Timeout(Exception):
+
+    def __init__(self, error_message):
+        self.error_message = error_message
+
+    def __str__(self):
+        return "%s" % self.error_message
+
+
+class InvalidURL(Exception):
+
+    def __init__(self, error_message):
+        self.error_message = error_message
+
+    def __str__(self):
+        return "%s" % self.error_message
+
+
+class NotFoundError(Exception):
+
+    def __init__(self, error_message):
+        self.error_message = error_message
+
+    def __str__(self):
+        return "%s" % self.error_message
