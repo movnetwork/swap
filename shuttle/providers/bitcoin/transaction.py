b39405ef7380a59ab009f0ad2fa17bb6915f5a1e
meherett <meherett@zoho.com>
2020-01-28 21:50:01 +0300

    added balance checker and update FundTransaction class

--- shuttle/providers/bitcoin/transaction.py
+++ shuttle/providers/bitcoin/transaction.py
@@ -14,6 +14,7 @@ from .solver import ClaimSolver, FundSolver, RefundSolver
 from .rpc import get_transaction_detail
 from .htlc import HTLC
 from .wallet import Wallet
+from ...utils.exceptions import BalanceError, ClientError
 
 
 class Transaction:
@@ -25,37 +26,39 @@ class Transaction:
         self.transaction = None
         # Bitcoin network
         self.network = network
+        # Bitcoin fee
+        self.fee = int()
         # Setting testnet
         setup(network, strict=True)
 
-    # Building transaction
-    def build_transaction(self, previous_transaction_inputs: list, transaction_outputs: list, locktime=0):
-        # Building mutable bitcoin transaction
-        self.transaction = MutableTransaction(version=self.version, ins=previous_transaction_inputs,
-                                              outs=transaction_outputs, locktime=Locktime(locktime))
-        return self
-
-    # Signing transaction using private keys
-    def sign(self, previous_transaction_outputs: list, solver: list):
-        self.transaction.spend(previous_transaction_outputs, solver)
-        return self
+    # # Building transaction
+    # def build_transaction(self, inputs: list, outputs: list, locktime=0, **kwargs):
+    #     # Building mutable bitcoin transaction
+    #     self.transaction = MutableTransaction(version=self.version, ins=inputs,
+    #                                           outs=outputs, locktime=Locktime(locktime))
+    #     return self
+    #
+    # # Signing transaction
+    # def sign(self, outputs: list, solver: list):
+    #     self.transaction.spend(outputs, solver)
+    #     return self
 
     # Transaction hash
     def hash(self):
         if self.transaction is None:
-            raise ValueError("Transaction script is none, Please build transaction first.")
+            raise ValueError("transaction script is none, Please build transaction first.")
         return self.transaction.txid
 
     # Transaction json format
     def json(self):
         if self.transaction is None:
-            raise ValueError("Transaction script is none, Please build transaction first.")
+            raise ValueError("transaction script is none, Please build transaction first.")
         return self.transaction.to_json()
 
     # Transaction raw
     def raw(self):
         if self.transaction is None:
-            raise ValueError("Transaction script is none, Please build transaction first.")
+            raise ValueError("transaction script is none, Please build transaction first.")
         return self.transaction.hexlify()
 
     @staticmethod
@@ -82,24 +85,35 @@ class Transaction:
 
 class FundTransaction(Transaction):
 
-    def __init__(self, wallet: Wallet, htlc: HTLC, amount: int, network="testnet", version=2):
-        super().__init__(network=network, version=version)
-        # Bitcoin sender wallet
-        assert isinstance(wallet, Wallet), "Invalid Bitcoin Wallet!"
-        self.wallet = wallet
-        # Bitcoin sender hash time lock contract
-        assert isinstance(htlc, HTLC), "Invalid Bitcoin HTLC script!"
-        self.htlc = htlc
-        # Bitcoin sender fund amount
-        self.amount = amount
-        # Bitcoin sender unspent transactions
-        self.unspent = self.wallet.unspent()
-        # Get previous transaction indexes using funding amount
-        self.previous_transaction_indexes = self.get_previous_transaction_indexes()
+    def __init__(self, version=2, network="testnet"):
+        super().__init__(version=version, network=network)
+        # Initialization wallet, htlc, amount and unspent
+        self.wallet, self.htlc, self.amount, self.unspent = None, None, None, None
+        # Getting previous transaction indexes using funding amount
+        self.previous_transaction_indexes = None
 
     # Building transaction
-    def build_transaction(self, locktime=0, **kwargs):
+    def build_transaction(self, wallet: Wallet, htlc: HTLC, amount: int, locktime=0):
+        # Checking build transaction arguments instance
+        if not isinstance(wallet, Wallet):
+            raise TypeError("invalid wallet instance, only takes bitcoin Wallet class")
+        if not isinstance(htlc, HTLC):
+            raise TypeError("invalid htlc instance, only takes bitcoin HTLC class")
+        if not isinstance(amount, int):
+            raise TypeError("invalid amount instance, only takes integer type")
+        # Setting wallet, htlc, amount and unspent
+        self.wallet, self.htlc, self.amount = wallet, htlc, amount
+        # Getting unspent transaction output
+        self.unspent = self.wallet.unspent()
+        # Setting previous transaction indexes
+        self.previous_transaction_indexes = \
+            self.get_previous_transaction_indexes(amount=self.amount)
+        # Getting transaction inputs and amount
         inputs, amount = self.inputs(self.unspent, self.previous_transaction_indexes)
+        # Calculating bitcoin fee
+        self.fee = fee_calculator(len(inputs), 2)
+        if amount < (self.amount + self.fee):
+            raise BalanceError("insufficient spend utxos")
         # Building mutable bitcoin transaction
         self.transaction = MutableTransaction(
             version=self.version, ins=inputs,
@@ -108,15 +122,17 @@ class FundTransaction(Transaction):
                 TxOut(value=self.amount, n=0,
                       script_pubkey=P2shScript.unhexlify(self.htlc.hash())),
                 # Controlling amounts when we are funding on htlc script.
-                TxOut(value=amount - (fee_calculator(len(inputs), 2) + self.amount), n=1,
+                TxOut(value=amount - (self.fee + self.amount), n=1,
                       script_pubkey=P2pkhScript.unhexlify(self.wallet.p2pkh()))
             ], locktime=Locktime(locktime))
         return self
 
-    # Signing transaction using private keys
-    def sign(self, solver: FundSolver, **kwargs):
+    # Signing transaction using fund solver
+    def sign(self, solver: FundSolver):
         if not isinstance(solver, FundSolver):
-            raise Exception("Solver error")
+            raise TypeError("invalid solver instance, only takes bitcoin FundSolver class")
+        if not self.unspent or not self.previous_transaction_indexes or not self.transaction:
+            raise ValueError("transaction script or unspent is none, build transaction first")
         outputs = self.outputs(self.unspent, self.previous_transaction_indexes)
         self.transaction.spend(outputs, [solver.solve() for _ in outputs])
         return self
@@ -137,14 +153,14 @@ class FundTransaction(Transaction):
 
     def unsigned_raw(self):
         outputs = list()
-        if self.transaction is None:
-            raise ValueError("Transaction script is none, Please build transaction first.")
+        if not self.transaction or not self.unspent:
+            raise ValueError("transaction script or unspent is none, build transaction first")
         for index, utxo in enumerate(self.unspent):
             if self.previous_transaction_indexes is None or index in self.previous_transaction_indexes:
                 outputs.append(dict(amount=utxo["amount"],
                                n=utxo["output_index"], script=utxo["script"]))
         return b64encode(str(json.dumps(dict(
-            raw=self.transaction.hexlify(), outputs=outputs, type="fund_unsigned"
+            fee=self.fee, raw=self.transaction.hexlify(), outputs=outputs, type="fund_unsigned"
         ))).encode()).decode()
 
 
@@ -170,6 +186,8 @@ class ClaimTransaction(Transaction):
 
     # Building transaction
     def build_transaction(self, locktime=0, **kwargs):
+        # Calculating fee
+        self.fee = fee_calculator(1, 1)
         # Building mutable bitcoin transaction
         self.transaction = MutableTransaction(
             version=self.version,
@@ -205,8 +223,8 @@ class ClaimTransaction(Transaction):
             raise ValueError("Transaction script is none, Please build transaction first.")
         outputs = [dict(amount=self.htlc_value, n=0, script=self.htlc["script"])]
         return b64encode(str(json.dumps(dict(
-            raw=self.transaction.hexlify(), outputs=outputs, type="claim_unsigned",
-            recipient=str(self.wallet.address()), sender=str(self.sender_address)
+            fee=self.fee, raw=self.transaction.hexlify(), outputs=outputs, type="claim_unsigned",
+            recipient_address=str(self.wallet.address()), sender_address=str(self.sender_address)
         ))).encode()).decode()
 
 
@@ -232,6 +250,8 @@ class RefundTransaction(Transaction):
 
     # Building transaction
     def build_transaction(self, locktime=0, **kwargs):
+        # Calculating fee
+        self.fee = fee_calculator(1, 1)
         # Building mutable bitcoin transaction
         self.transaction = MutableTransaction(
             version=self.version,
@@ -240,7 +260,7 @@ class RefundTransaction(Transaction):
                      script_sig=ScriptSig.empty(), sequence=Sequence.max())
             ],
             outs=[
-                TxOut(value=self.htlc_value - fee_calculator(1, 1), n=0,
+                TxOut(value=self.htlc_value - self.fee, n=0,
                       script_pubkey=P2pkhScript.unhexlify(self.wallet.p2pkh()))
             ], locktime=Locktime(locktime))
         return self
@@ -267,6 +287,6 @@ class RefundTransaction(Transaction):
             raise ValueError("Transaction script is none, Please build transaction first.")
         outputs = [dict(amount=self.htlc_value, n=0, script=self.htlc["script"])]
         return b64encode(str(json.dumps(dict(
-            raw=self.transaction.hexlify(), outputs=outputs, type="refund_unsigned",
-            recipient=str(self.wallet.address()), sender=str(self.sender_address)
+            fee=self.fee, raw=self.transaction.hexlify(), outputs=outputs, type="refund_unsigned",
+            recipient_address=str(self.wallet.address()), sender_address=str(self.sender_address)
         ))).encode()).decode()
