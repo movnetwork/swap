a6568d9824208a12277dbeb958142f1110965749
meherett <meherett@zoho.com>
2020-02-14 05:08:54 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,19 @@ with open("requirements.txt", "r") as _requirements:
 
 setup(
     name="pyshuttle",
-    version="0.0.1",
+    version="0.0.3.dev1",
     description="Cross-chain atomic swaps between the networks of two cryptocurrencies.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license="AGPLv3+",
+    license="AGPL-3.0",
     author="Meheret Tesfaye",
     author_email="meherett@zoho.com",
     url="https://github.com/mehetett/pyshuttle",
     packages=find_packages(),
+    keywords=['shuttle'],
+    entry_points={
+        'console_scripts': ["shuttle=shuttle.shell.__main__:main"]
+    },
     python_requires=">=3.5,<4",
     install_requires=requirements,
     classifiers=[
