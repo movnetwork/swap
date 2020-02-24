c854ec96cb2e5c644e3e67bbeed4dccdd6a1e760
meherett <meherett@zoho.com>
2020-02-24 07:30:27 -0800

    change cli tool to click library

--- shuttle/cli/__main__.py
+++ shuttle/cli/__main__.py
@@ -2,93 +2,59 @@
 # coding=utf-8
 
 # IMPORT ALL PACKAGES
-from shuttle import __version__, __author__, __author_email__
-from shuttle.cli import *
+from shuttle import __version__
+from shuttle.cli import click
+# IMPORT PROVIDERS
+from shuttle.cli.providers.bitcoin import bitcoin
+from shuttle.cli.providers.bytom import bytom
 
-description = textwrap.dedent("""
-    PyShuttle \n
-    Cross-chain atomic swaps between the networks of two cryptocurrencies
-""")
-
-information = textwrap.dedent("""
-    DONATION
-    BITCOIN 3LLtTQaGV8bfvqtaeJinitzob8Y8CvryA7
-    BYTOM bm1qzx7pjr6whcaxmh9u0thkjuavf2ynk3zkgshhle
-    
-    AUTHOR %s
-    AUTHOR EMAIL %s
-""" % (__author__, __author_email__))
+CONTEXT_SETTINGS = dict(
+    help_option_names=["-h", "--help"],
+)
 
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv[1:]
-    parser = argparse.ArgumentParser(
-        prog="shuttle",
-        usage="[providers] [-h, --help] [-v, --version]",
-        description=description,
-        epilog=information
-    )
-    # Set parser error
-    parser.error = lambda message: \
-        print(parser.format_usage() + "error: " + message)
+class AliasedGroup(click.Group):
 
-    parser.add_argument("-v", "--version", action="version",
-                        version="PyShuttle version %s" % __version__,
-                        help="show pyshuttle version number and exit")
+    def get_command(self, ctx, cmd_name):
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+        matches = [x for x in self.list_commands(ctx)
+                   if x.startswith(cmd_name)]
+        if not matches:
+            return None
+        elif len(matches) == 1:
+            return click.Group.get_command(self, ctx, matches[0])
+        ctx.fail('Too many matches: %s' % ', '.join(sorted(matches)))
 
-    shuttle_subparsers = parser.add_subparsers(
-        title="shuttle providers",
-        metavar="btc, bitcoin          bitcoin cryptocurrency" + "\n  " +
-                "btm, bytom            bytom cryptocurrency"
-    )
 
-    shuttle_parser_bitcoin = shuttle_subparsers.add_parser(
-        name="bitcoin", usage="[arguments] [-h, --help]")
-    shuttle_parser_bitcoin.error = lambda message: print("error: " + message)
-    shuttle_subparsers_bitcoin = shuttle_parser_bitcoin.add_subparsers(
-        title="bitcoin arguments",
-        metavar="sign, signature       to sign bitcoin unsigned transaction raw"
-    )
-    shuttle_parser_bytom = shuttle_subparsers.add_parser(
-        name="bytom", usage="[arguments] [-h, --help]")
-    shuttle_subparsers_bytom = shuttle_parser_bytom.add_subparsers(
-        title="bytom arguments",
-        metavar="sign, signature       to sign bytom unsigned transaction raw"
-    )
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo("PyShuttle version %s" % __version__)
+    ctx.exit()
 
-    shuttle_parser_bitcoin_sign = shuttle_subparsers_bitcoin.add_parser(
-        name="sign", usage="[-p, --private] [-u, -unsigned] [-f, --file] [-s, --save]")
-    shuttle_parser_bitcoin_sign.error = lambda message: \
-        print(shuttle_parser_bitcoin_sign.format_usage() + "error: " + message)
-    shuttle_parser_bitcoin_sign.add_argument("-p", "--private", dest="private", action="store", required=True,
-                                             help="bitcoin private key")
-    shuttle_parser_bitcoin_sign.add_argument("-u", "--unsigned", action="store", default=None,
-                                             help="bitcoin unsigned transaction raw")
-    shuttle_parser_bitcoin_sign.add_argument("-f", "--file", type=argparse.FileType("r"),
-                                             help="unsigned transaction raw file")
-    shuttle_parser_bitcoin_sign.add_argument("-s", "--save", action="store", default=None,
-                                             help="save signed transaction raw")
 
-    shuttle_parser_bytom_sign = shuttle_subparsers_bytom.add_parser(
-        name="sign", usage="[-xp, --xprivate] [-u, -unsigned] [-f, --file] [-s, --save]")
-    shuttle_parser_bytom_sign.add_argument("-xp", "--xprivate", action="store", required=True,
-                                           help="bytom xprivate key")
-    shuttle_parser_bytom_sign.add_argument("-u", "--unsigned", action="store", default=None,
-                                           help="bytom unsigned transaction raw")
-    shuttle_parser_bytom_sign.add_argument("-f", "--file", type=argparse.FileType("r"),
-                                           help="unsigned transaction raw file")
-    shuttle_parser_bytom_sign.add_argument("-s", "--save", action="store", default=None,
-                                           help="save signed transaction raw")
+@click.group(cls=AliasedGroup,
+             options_metavar="[OPTIONS]", context_settings=CONTEXT_SETTINGS)
+@click.option("-v", "--version", is_flag=True, callback=print_version,
+              expose_value=False, help="Show Shuttle version and exit.")
+def shuttle():
+    """
+    SHUTTLE CLI
 
-    # try:
-    parse_args = parser.parse_args()
+    Cross-chain atomic swaps between the networks of two cryptocurrencies.
 
-    if not argv:
-        print("usage: [providers] [-h, --help] [-v, --version]")
+    LICENCE AGPL-3.0 | AUTHOR Meheret Tesfaye | EMAIL meherett@zoho.com
+    """
+    pass
 
-        # elif parse_args.help:
-        # parser.print_help()
 
-    # except Exception as exception:
-    #     parser.error(str(exception))
+# Adding bitcoin provider
+shuttle.add_command(bitcoin)
+# Adding bytom provider
+shuttle.add_command(bytom)
+
+
+def main():
+    shuttle()
