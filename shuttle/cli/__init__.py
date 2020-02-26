849e53ed470dbc62b1c5d8d5ba3d40bbf599d7e7
meherett <meherett@zoho.com>
2020-02-25 23:09:42 -0800

    Upgrade bitcoin cli

--- shuttle/cli/__init__.py
+++ shuttle/cli/__init__.py
@@ -5,8 +5,29 @@ import click
 import sys
 
 
+# Success template
+def success(_):
+    return "[{}] {}".format(
+        click.style("SUCCESS", fg="green"), str(_))
+
+
+# Warning template
+def warning(_):
+    return "[{}] {}".format(
+        click.style("WARNING", fg="yellow"), str(_))
+
+
+# Error template
+def error(_):
+    return "[{}] {}".format(
+        click.style("ERROR", fg="red"), str(_))
+
+
 __all__ = [
     "textwrap",
     "click",
-    "sys"
+    "sys",
+    "success",
+    "warning",
+    "error"
 ]
--- shuttle/cli/providers/bitcoin/signature.py
+++ shuttle/cli/providers/bitcoin/signature.py
@@ -4,8 +4,10 @@
 from base64 import b64encode, b64decode
 
 import json
+import sys
+import binascii
 
-from shuttle.cli import click
+from shuttle.cli import click, success, warning, error
 from shuttle.providers.bitcoin.solver \
     import FundSolver, ClaimSolver, RefundSolver
 from shuttle.providers.bitcoin.signature \
@@ -21,49 +23,84 @@ VERSION = 2  # 1
                short_help="Select bitcoin transaction raw signer.")
 @click.option("-p", "--private", type=str, required=True, help="Set bitcoin private key.")
 @click.option("-u", "--unsigned", type=str, required=True, help="Set bitcoin unsigned transaction raw.")
-@click.option("-n", "--network", type=str, default=NETWORK,
-              help="Set bitcoin network.", show_default=True)
+@click.option("-s", "--secret", type=str, default=str(), help="Set secret key.")
 @click.option("-v", "--version", type=int, default=VERSION,
               help="Set bitcoin version.", show_default=True)
-def sign(private, unsigned, network, version):
+def sign(private, unsigned, secret, version):
     """
     SHUTTLE BITCOIN SIGN
     """
 
     unsigned_raw = str(unsigned + "=" * (-len(unsigned) % 4))
-    transaction = json.loads(b64decode(unsigned_raw.encode()).decode())
+    try:
+        transaction = json.loads(b64decode(unsigned_raw.encode()).decode())
+    except (binascii.Error, json.decoder.JSONDecodeError) as _error:
+        click.echo(error("invalid unsigned transaction raw"))
+        sys.exit()
     if "type" not in transaction:
-        raise ValueError("invalid unsigned transaction raw")
+        click.echo(warning("there is no type provided on unsigned transaction raw"))
+        click.echo(error("invalid unsigned transaction raw"))
+        sys.exit()
+    if "network" not in transaction:
+        click.echo(warning("there is no network provided on unsigned transaction raw"))
+        click.echo(error("invalid unsigned transaction raw"))
+        sys.exit()
 
     if transaction["type"] == "bitcoin_fund_unsigned":
         # Fund HTLC solver
+        if len(private) != 64:
+            click.echo(error("invalid bitcoin private key"))
+            sys.exit()
         fund_solver = FundSolver(private_key=private)
-
-        fund_signature = FundSignature(network=network, version=version)
-        fund_signature.sign(unsigned_raw=unsigned_raw, solver=fund_solver)
-        click.echo(fund_signature.signed_raw())
+        try:
+            # Fund signature
+            fund_signature = FundSignature(network=transaction["network"], version=version)
+            fund_signature.sign(unsigned_raw=unsigned_raw, solver=fund_solver)
+            click.echo(success(fund_signature.signed_raw()))
+        except Exception as exception:
+            click.echo(error(exception))
+            sys.exit()
 
     elif transaction["type"] == "bitcoin_claim_unsigned":
-        if "secret" not in transaction or transaction["secret"] is None:
-            raise ValueError("invalid claim unsigned transaction raw, empty secret key")
+        if secret != str():
+            _secret = secret
+        elif "secret" not in transaction or transaction["secret"] is None:
+            click.echo(warning("secret key is empty, use -s or --secret \"Hello Meheret!\""))
+            _secret = str()
+        else:
+            _secret = transaction["secret"]
         # Claim HTLC solver
         claim_solver = ClaimSolver(
-            secret=transaction["secret"],
+            secret=_secret,
             private_key=private
         )
-        # Claim signature
-        claim_signature = ClaimSignature(network=network, version=version)
-        claim_signature.sign(unsigned_raw=unsigned_raw, solver=claim_solver)
-        click.echo(claim_signature.signed_raw())
+        try:
+            # Claim signature
+            claim_signature = ClaimSignature(network=transaction["network"], version=version)
+            claim_signature.sign(unsigned_raw=unsigned_raw, solver=claim_solver)
+            click.echo(success(claim_signature.signed_raw()))
+        except Exception as exception:
+            click.echo(error(exception))
+            sys.exit()
 
     elif transaction["type"] == "bitcoin_refund_unsigned":
-        if "secret" not in transaction or transaction["secret"] is None:
-            raise ValueError("invalid refund unsigned transaction raw, empty secret key")
+        if secret != str():
+            _secret = secret
+        elif "secret" not in transaction or transaction["secret"] is None:
+            click.echo(warning("secret key is empty, use -s or --secret \"Hello Meheret!\""))
+            _secret = str()
+        else:
+            _secret = transaction["secret"]
         # Refunding HTLC solver
         refund_solver = RefundSolver(
-            secret=transaction["secret"],
+            secret=_secret,
             private_key=private
         )
-        refund_signature = RefundSignature(network=network, version=version)
-        refund_signature.sign(unsigned_raw=unsigned_raw, solver=refund_solver)
-        click.echo(refund_signature.signed_raw())
+        try:
+            # Refund signature
+            refund_signature = RefundSignature(network=transaction["network"], version=version)
+            refund_signature.sign(unsigned_raw=unsigned_raw, solver=refund_solver)
+            click.echo(success(refund_signature.signed_raw()))
+        except Exception as exception:
+            click.echo(error(exception))
+            sys.exit()
