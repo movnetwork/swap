bf45de8073e19b7e6d917feb9e691f2eaf93c8df
meherett <meherett@zoho.com>
2020-03-01 09:28:14 +0300

    Upgrade version 0.0.5b1 to 0.1.0.dev1

--- shuttle/__init__.py
+++ shuttle/__init__.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
-# PyShuttle Information's
-__version__ = "0.0.5b1"
+
+from .shuttle import Provider
+from .providers import bitcoin, bytom, config
+
+# Shuttle Information's
+__version__ = "0.1.0.dev1"
 __author__ = "Meheret Tesfaye"
-__author_email__ = "meherett@zoho.com"
+__email__ = "meherett@zoho.com"
 
 # Cryptocurrency name
 Bitcoin = "bitcoin"
@@ -13,7 +17,11 @@ Bytom = "bytom"
 __all__ = [
     "__version__",
     "__author__",
-    "__author_email__",
+    "__email__",
+    "Provider",
     "Bitcoin",
-    "Bytom"
+    "Bytom",
+    "bitcoin",
+    "bytom",
+    "config"
 ]
