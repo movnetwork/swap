0352c020cf87454b8bace06af1b744bbf0d59447
meherett <meherett@zoho.com>
2020-03-26 15:31:20 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,6 +26,7 @@ author = 'Meheret Tesfaye'
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
