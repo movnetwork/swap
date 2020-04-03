80ea2351a07382f815a48b2194a79c8cf389c9f3
meherett <meherett@zoho.com>
2020-04-03 11:35:44 +0300

    Added test_bitcoin_wallet_tools function.

--- tests/providers/bitcoin/test_bitcoin_wallet.py
+++ tests/providers/bitcoin/test_bitcoin_wallet.py
@@ -5,14 +5,15 @@ from shuttle.providers.bitcoin.wallet import Wallet
 
 def test_from_passphrase():
     passphrase = "meheret tesfaye batu bayou".encode()
-    print("Passphrase", passphrase)
-
     # Initialize bitcoin wallet
-    bitcoin_from_private_key = Wallet(network="testnet") \
+    bitcoin_from_passphrase = Wallet(network="testnet") \
         .from_passphrase(passphrase)
 
+    bitcoin_from_private_key = Wallet(network="testnet") \
+        .from_private_key("92cbbc5990cb5090326a76feeb321cad01048635afe5756523bbf9f7a75bf38b")
+
     private_key = "92cbbc5990cb5090326a76feeb321cad01048635afe5756523bbf9f7a75bf38b"
-    assert bitcoin_from_private_key.private_key() == private_key
+    assert bitcoin_from_private_key.private_key() == private_key == bitcoin_from_passphrase.private_key()
 
     public_key = "03c56a6005d4a8892d28cc3f7265e5685b548627d59108973e474c4e26f69a4c84"
     assert bitcoin_from_private_key.public_key() == public_key
@@ -120,3 +121,29 @@ def test_from_address():
 
     p2sh = "a914347283eee92ad685909044619adaa70370b2538787"
     assert bitcoin_from_address.p2sh() == p2sh
+
+
+def test_bitcoin_wallet_tools():
+    wallet = Wallet(network="testnet")
+    private_key = "92cbbc5990cb5090326a76feeb321cad01048635afe5756523bbf9f7a75bf38b"
+    public_key = wallet.public_key(private_key=private_key)
+    assert public_key == "03c56a6005d4a8892d28cc3f7265e5685b548627d59108973e474c4e26f69a4c84"
+    public_key = "04c56a6005d4a8892d28cc3f7265e5685b548627d59108973e474c4e26f69a4c84fee63d89a5979801c96" \
+                 "5994963c77bfb470dff5afd351a442ebf329f3b2c2835"
+
+    compressed = wallet.compressed(public_key=public_key)
+    assert compressed == "03c56a6005d4a8892d28cc3f7265e5685b548627d59108973e474c4e26f69a4c84"
+    uncompressed = wallet.uncompressed(public_key=public_key)
+    assert uncompressed == public_key
+    address = wallet.address(public_key=uncompressed)
+    assert address == "mqLyrNDjpENRMZAoDpspH7kR9RtgvhWzYE"
+    _hash = wallet.hash(public_key=uncompressed)
+    assert _hash == "6bce65e58a50b97989930e9a4ff1ac1a77515ef1"
+    p2pkh = wallet.p2pkh(address=address)
+    assert p2pkh == "76a9146bce65e58a50b97989930e9a4ff1ac1a77515ef188ac"
+    p2sh = wallet.p2sh(address=address)
+    assert p2sh == "a914347283eee92ad685909044619adaa70370b2538787"
+    balance = wallet.balance(address=address, network="testnet")
+    assert isinstance(balance, int)
+    unspent = wallet.unspent(address=address, network="testnet", limit=1)
+    assert isinstance(unspent, list)
