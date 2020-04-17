605536f90770e65c2282a6e66b7858509e4b23fd
meherett <meherett@zoho.com>
2020-04-17 19:16:56 +0300

    Added network on bytom htlc cli.

--- shuttle/cli/providers/bytom/htlc.py
+++ shuttle/cli/providers/bytom/htlc.py
@@ -9,15 +9,16 @@ from shuttle.providers.bytom.htlc import HTLC
 
 
 @click.command("htlc", options_metavar="[OPTIONS]",
-               short_help="Select Bitcoin Hash Time Lock Contract (HTLC) builder.")
+               short_help="Select Bytom Hash Time Lock Contract (HTLC) builder.")
 @click.option("-sh", "--secret-hash", type=str, required=True, help="Set secret 256 hash.")
-@click.option("-rp", "--recipient-public", type=str, required=True, help="Set Bitcoin recipient public key.")
-@click.option("-sp", "--sender-public", type=str, required=True, help="Set Bitcoin sender public key.")
-@click.option("-s", "--sequence", type=int, default=100, help="Set Bitcoin expiration block (sequence).")
-def htlc(secret_hash, recipient_public, sender_public, sequence):
+@click.option("-rp", "--recipient-public", type=str, required=True, help="Set Bytom recipient public key.")
+@click.option("-sp", "--sender-public", type=str, required=True, help="Set Bytom sender public key.")
+@click.option("-s", "--sequence", type=int, default=100, help="Set Bytom expiration block (sequence).")
+@click.option("-n", "--network", type=str, default="testnet", help="Set Bytom network.")
+def htlc(secret_hash, recipient_public, sender_public, sequence, network):
     try:
         click.echo(
-            HTLC().init(
+            HTLC(network=network).init(
                 secret_hash=secret_hash,
                 recipient_public=recipient_public,
                 sender_public=sender_public,
