50e84c5d44bbb914145081f2f8bcc3e14b71d074
meherett <meherett@zoho.com>
2020-04-17 19:05:30 +0300

    Update bitcoin htlc tester.

--- tests/providers/bitcoin/test_bitcoin_htlc.py
+++ tests/providers/bitcoin/test_bitcoin_htlc.py
@@ -2,13 +2,16 @@
 
 from shuttle.providers.bitcoin.htlc import HTLC
 from shuttle.utils import sha256
+from shuttle.utils.exceptions import AddressError
+
+import pytest
 
 
 # Testing HTLC init
 def test_htlc_init():
     htlc = HTLC(network="testnet")
 
-    htlc.init(secret_hash=sha256("BooOoom!".encode()), recipient_address="mrmtGq2HMmqAogSsGDjCtXUpxrb7rHThFH",
+    htlc.init(secret_hash=sha256("BooOoom!".encode()).hex(), recipient_address="mrmtGq2HMmqAogSsGDjCtXUpxrb7rHThFH",
               sender_address="mqLyrNDjpENRMZAoDpspH7kR9RtgvhWzYE", sequence=5)
 
     htlc_bytecode = htlc.bytecode()
@@ -56,6 +59,9 @@ def test_htlc_from_bytecode():
     htlc_hash = htlc.hash()
     assert str(htlc_hash) == "a914971894c58d85981c16c2059d422bcde0b156d04487"
 
+    with pytest.raises(TypeError, match="bytecode must be string format"):
+        HTLC(network="testnet").from_bytecode(float())
+
 
 # Testing HTLC from opcode
 def test_htlc_from_opcode():
@@ -84,3 +90,56 @@ def test_htlc_from_opcode():
 
     htlc_hash = htlc.hash()
     assert str(htlc_hash) == "a914971894c58d85981c16c2059d422bcde0b156d04487"
+
+    with pytest.raises(TypeError, match="op_code must be string format"):
+        HTLC(network="mainnet").from_opcode(bool())
+
+
+def test_bytom_htlc_init_validation():
+
+    with pytest.raises(ValueError, match="invalid network, only mainnet or testnet"):
+        HTLC(network="unknown")
+
+    with pytest.raises(ValueError, match="htlc script is none, initialization htlc first"):
+        HTLC(network="testnet").bytecode()
+
+    with pytest.raises(ValueError, match="htlc script is none, initialization htlc first"):
+        HTLC(network="testnet").opcode()
+
+    with pytest.raises(ValueError, match="htlc script is none, initialization htlc first"):
+        HTLC(network="testnet").hash()
+
+    with pytest.raises(ValueError, match="htlc script is none, initialization htlc first"):
+        HTLC(network="testnet").address()
+
+    with pytest.raises(TypeError, match="secret hash must be string format"):
+        HTLC(network="mainnet").init(int(), str(), str(), int())
+
+    with pytest.raises(ValueError, match="invalid secret hash, length must be 64."):
+        HTLC(network="mainnet").init(str(), str(), str(), int())
+
+    with pytest.raises(TypeError, match="recipient address must be string format"):
+        HTLC(network="mainnet").init("3a26da82ead15a80533a02696656b14b5dbfd84eb14790f2e1be5e9e45820eeb",
+                                     float(), str(), int())
+
+    with pytest.raises(AddressError, match=r"invalid .* recipient .* address"):
+        HTLC(network="mainnet").init("3a26da82ead15a80533a02696656b14b5dbfd84eb14790f2e1be5e9e45820eeb",
+                                     "2N729UBGZB3xjsGFRgKivy4bSjkaJGMVSpB",
+                                     str(), int())
+
+    with pytest.raises(TypeError, match="sender address must be string format"):
+        HTLC(network="testnet").init("3a26da82ead15a80533a02696656b14b5dbfd84eb14790f2e1be5e9e45820eeb",
+                                     "2N729UBGZB3xjsGFRgKivy4bSjkaJGMVSpB",
+                                     bool(), int())
+
+    with pytest.raises(AddressError, match=r"invalid .* sender .* address"):
+        HTLC(network="mainnet").init("3a26da82ead15a80533a02696656b14b5dbfd84eb14790f2e1be5e9e45820eeb",
+                                     "1EzQPm2RFqtY7fGpcjVkXvQ9xzEKhDZmWP",
+                                     "muTnffLDR5LtFeLR2i3WsKVfdyvzfyPnVB",
+                                     int())
+
+    with pytest.raises(TypeError, match="sequence must be integer format"):
+        HTLC(network="testnet").init("3a26da82ead15a80533a02696656b14b5dbfd84eb14790f2e1be5e9e45820eeb",
+                                     "2N729UBGZB3xjsGFRgKivy4bSjkaJGMVSpB",
+                                     "muTnffLDR5LtFeLR2i3WsKVfdyvzfyPnVB",
+                                     str())
