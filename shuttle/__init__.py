f642e804684e4ef655bb6d85e7129bbfbdcfd2a9
meherett <meherett@zoho.com>
2020-04-21 21:42:07 +0300

    Upgrade v0.2.0b1 to v0.2.0b2

--- shuttle/__init__.py
+++ shuttle/__init__.py
@@ -4,7 +4,7 @@ from .providers import bitcoin, bytom, config
 
 
 # Shuttle Information's
-__version__ = "0.2.0b1"
+__version__ = "0.2.0b2"
 __license__ = "AGPL-3.0"
 __author__ = "Meheret Tesfaye"
 __email__ = "meherett@zoho.com"
