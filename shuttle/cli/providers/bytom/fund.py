9effdaa1effa6869ff578179024e329bc3c0bc81
meherett <meherett@zoho.com>
2020-04-21 21:40:39 +0300

    Added asset parameter.

--- shuttle/cli/providers/bytom/fund.py
+++ shuttle/cli/providers/bytom/fund.py
@@ -14,15 +14,17 @@ from shuttle.providers.bytom.htlc import HTLC
                short_help="Select Bytom unsigned transaction builder.")
 @click.option("-sg", "--sender-guid", type=str, required=True, help="Set Bytom sender GUID.")
 @click.option("-a", "--amount", type=int, required=True, help="Set Bytom amount to fund on HTLC.")
+@click.option("-as", "--asset", type=str, required=True, help="Set Bytom asset id.")
 @click.option("-b", "--bytecode", type=str, required=True, help="Set Bytom HTLC bytecode.")
 @click.option("-n", "--network", type=str, default="testnet", help="Set Bytom network.")
-def fund(sender_guid, amount, bytecode, network):
+def fund(sender_guid, amount, asset, bytecode, network):
     try:
         click.echo(
             FundTransaction(network=network).build_transaction(
                 wallet=Wallet(network=network).from_guid(guid=sender_guid),
                 htlc=HTLC(network=network).from_bytecode(bytecode=bytecode),
-                amount=int(amount)
+                amount=int(amount),
+                asset=asset
             ).unsigned_raw()
         )
     except Exception as exception:
