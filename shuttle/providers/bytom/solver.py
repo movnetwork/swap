fc61b23fae878c050d3c7ae48d3b322c2ef17ab7
meherett <meherett@zoho.com>
2020-06-14 09:23:51 +0300

    Fix solver docs.

--- shuttle/providers/bytom/solver.py
+++ shuttle/providers/bytom/solver.py
@@ -30,12 +30,12 @@ class FundSolver:
     :returns:  FundSolver -- Bytom fund solver instance.
 
     >>> from shuttle.providers.bytom.solver import FundSolver
-    >>> fund_solver = FundSolver(sender_xprivate_key)
+    >>> fund_solver = FundSolver(xprivate_key="205b15f70e253399da90b127b074ea02904594be9d54678207872ec1ba31ee51ef4490504bd2b6f997113671892458830de09518e6bd5958d5d5dd97624cfa4b")
     <shuttle.providers.bytom.solver.FundSolver object at 0x03FCCA60>
     """
 
-    def __init__(self, xprivate_key, account=1,
-                 change=False, address=1, path=None, indexes=None):
+    def __init__(self, xprivate_key,
+                 account=1, change=False, address=1, path=None, indexes=None):
         # Checking path and indexes
         if not path and not indexes:
             path = f"m/44/153/{account}/{1 if change else 0}/{address}"
@@ -81,7 +81,8 @@ class ClaimSolver:
     :returns:  ClaimSolver -- Bytom claim solver instance.
 
     >>> from shuttle.providers.bytom.solver import ClaimSolver
-    >>> claim_solver = ClaimSolver("Hello Meheret!", recipient_xprivate_key)
+    >>> recipient_xprivate_key = "205b15f70e253399da90b127b074ea02904594be9d54678207872ec1ba31ee51ef4490504bd2b6f997113671892458830de09518e6bd5958d5d5dd97624cfa4b"
+    >>> claim_solver = ClaimSolver(xprivate_key=recipient_xprivate_key, secret="Hello Meheret!", recipient_public="3e0a377ae4afa031d4551599d9bb7d5b27f4736d77f78cac4d476f0ffba5ae3e", sender_public="91ff7f525ff40874c4f47f0cab42e46e3bf53adad59adef9558ad1b6448f22e2", sequence=1000)
     <shuttle.providers.bytom.solver.ClaimSolver object at 0x03FCCA60>
     """
 
@@ -115,8 +116,11 @@ class ClaimSolver:
     # HTLC witnesses bytecode
     def witness(self, network=bytom["network"], use_script=False):
         return HTLC(network=network).init(
-            secret_hash=self.htlc_args[0], recipient_public=self.htlc_args[1],
-            sender_public=self.htlc_args[2], sequence=self.htlc_args[3], use_script=use_script
+            secret_hash=self.htlc_args[0],
+            recipient_public=self.htlc_args[1],
+            sender_public=self.htlc_args[2],
+            sequence=self.htlc_args[3],
+            use_script=use_script
         ).bytecode()
 
 
@@ -148,7 +152,8 @@ class RefundSolver:
     :returns:  RefundSolver -- Bytom refund solver instance.
 
     >>> from shuttle.providers.bytom.solver import RefundSolver
-    >>> refund_solver = RefundSolver(sender_xprivate_key)
+    >>> sender_xprivate_key = "205b15f70e253399da90b127b074ea02904594be9d54678207872ec1ba31ee51ef4490504bd2b6f997113671892458830de09518e6bd5958d5d5dd97624cfa4b"
+    >>> refund_solver = RefundSolver(xprivate_key=sender_xprivate_key, secret="Hello Meheret!", recipient_public="3e0a377ae4afa031d4551599d9bb7d5b27f4736d77f78cac4d476f0ffba5ae3e", sender_public="91ff7f525ff40874c4f47f0cab42e46e3bf53adad59adef9558ad1b6448f22e2", sequence=1000)
     <shuttle.providers.bytom.solver.RefundSolver object at 0x03FCCA60>
     """
 
@@ -182,6 +187,9 @@ class RefundSolver:
     # HTLC witnesses bytecode
     def witness(self, network=bytom["network"], use_script=False):
         return HTLC(network=network).init(
-            secret_hash=self.htlc_args[0], recipient_public=self.htlc_args[1],
-            sender_public=self.htlc_args[2], sequence=self.htlc_args[3], use_script=use_script
+            secret_hash=self.htlc_args[0],
+            recipient_public=self.htlc_args[1],
+            sender_public=self.htlc_args[2],
+            sequence=self.htlc_args[3],
+            use_script=use_script
         ).bytecode()
