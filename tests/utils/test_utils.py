7325bd6b7a2fe6da92e173632a9a289735aaef29
meherett <meherett@zoho.com>
2020-07-26 12:10:06 +0300

    Added more utils tests.

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -1,20 +1,63 @@
 #!/usr/bin/env python3
 
-from shuttle.utils import sha256, double_sha256, \
-    generate_mnemonic, generate_passphrase
-from mnemonic.mnemonic import Mnemonic
+from shuttle.utils import (
+    generate_passphrase, generate_entropy, generate_mnemonic,
+    is_mnemonic, get_mnemonic_language, sha256, double_sha256
+)
 
-mnemonic = Mnemonic(language="korean")
+import pytest
+
+
+MNEMONIC = "병아리 실컷 여인 축제 극히 저녁 경찰 설사 할인 해물 시각 자가용"
 
 
 def test_shuttle_utils():
-    assert sha256("meherett".encode()).hex() == \
-           "d4f5c55a45c004660b95ec833bb24569eba1559f214e90efa6e8d0b3afa14394"
 
-    assert double_sha256("meherett".encode()).hex() == \
-           "2803bf9ed1e5874825350b1b0753a96c00a99236b686bde337404453b11d3288"
+    assert len(generate_entropy(strength=128)) == 32
 
-    generated_mnemonic = generate_mnemonic(language="korean")
-    assert mnemonic.check(generated_mnemonic)
+    assert is_mnemonic("sceptre capter séquence girafe absolu relatif fleur zoologie muscle sirop saboter parure")
+
+    assert len(generate_mnemonic(language="chinese_traditional", strength=128).split(" ")) == 12
+
+    assert get_mnemonic_language(
+        mnemonic="sceptre capter séquence girafe absolu relatif fleur zoologie muscle sirop saboter parure"
+    ) == "french"
+
+    with pytest.raises(ValueError, match=r".*[128, 160, 192, 224, 256].*"):
+        assert len(generate_entropy(strength=129).split(" ")) == 12
+
+    assert is_mnemonic(mnemonic=MNEMONIC, language="korean")
+
+    with pytest.raises(ValueError, match=r"invalid language, .*"):
+        assert is_mnemonic(mnemonic=MNEMONIC, language="amharic")
+
+    assert not is_mnemonic(mnemonic=12341234, language="english")
+
+    with pytest.raises(ValueError, match="invalid 12 word mnemonic."):
+        assert get_mnemonic_language("1234 meheret tesfaye")
+
+    with pytest.raises(ValueError, match=r"invalid language, .*"):
+        assert generate_mnemonic(language="amharic")
+
+    with pytest.raises(ValueError, match=r"Strength should be one of the following .*"):
+        assert generate_mnemonic(strength=129)
 
     assert len(generate_passphrase(length=100)) == 100
+
+    assert sha256("meherett") == \
+        "d4f5c55a45c004660b95ec833bb24569eba1559f214e90efa6e8d0b3afa14394"
+
+    assert double_sha256("meherett") == \
+        "577f516220f7d5ff290264e50106210639ddadb3a6c97a7ea43bd1ee51b4d6be"
+
+    assert sha256("meherett".encode()) == \
+        "d4f5c55a45c004660b95ec833bb24569eba1559f214e90efa6e8d0b3afa14394"
+
+    assert double_sha256("meherett".encode()) == \
+        "577f516220f7d5ff290264e50106210639ddadb3a6c97a7ea43bd1ee51b4d6be"
+
+    with pytest.raises(TypeError, match="data must be str/bytes format!"):
+        sha256(int(123))
+
+    with pytest.raises(TypeError, match="data must be str/bytes format!"):
+        double_sha256(int(123))
