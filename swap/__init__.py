a846b135f4b93eea7f16433b929cd73b77511e96
meherett <meherett@zoho.com>
2020-09-04 11:03:45 +0300

    Add: Swap __description__

--- swap/__init__.py
+++ swap/__init__.py
@@ -6,11 +6,13 @@ __version__ = "0.1.0"
 __license__ = "AGPL-3.0"
 __author__ = "Meheret Tesfaye"
 __email__ = "meherett@zoho.com"
-
+__description__ = "Cross-chain atomic swap between the networks of two cryptocurrencies " \
+                  "based on Hash Time Lock Contracts (HTLCs) protocol."
 
 __all__ = [
     "__version__",
     "__license__",
     "__author__",
-    "__email__"
+    "__email__",
+    "__description__"
 ]
