8b62d92d548c0d2177938e35988ad2b19e183b73
meherett <meherett@zoho.com>
2020-09-19 16:33:38 +0300

    Change: HTLC init to build function.

--- cryptocurrencies/bitcoin/htlc.py
+++ cryptocurrencies/bitcoin/htlc.py
@@ -1,79 +1,38 @@
 #!/usr/bin/env python3
 
-from swap.providers.bitcoin.wallet import Wallet
 from swap.providers.bitcoin.htlc import HTLC
 from swap.providers.bitcoin.rpc import get_balance
 from swap.utils import sha256
 
-
 # Bitcoin network
-NETWORK = "testnet"
+NETWORK: str = "testnet"
 # Secret password/passphrase hash
-SECRET_HASH = sha256("Hello Meheret!")
+SECRET_HASH: str = sha256("Hello Meheret!")
 # Recipient Bitcoin address
-RECIPIENT_ADDRESS = "mwHXvCcug5Rn24c2rpgcRDSo3PyfxZJQQT"
+RECIPIENT_ADDRESS: str = "mwHXvCcug5Rn24c2rpgcRDSo3PyfxZJQQT"
 # Sender Bitcoin address
-SENDER_ADDRESS = "miAcLpYbaqE8KowBu2PwvqXG6y6vpQcfTJ"
+SENDER_ADDRESS: str = "miAcLpYbaqE8KowBu2PwvqXG6y6vpQcfTJ"
 # Expiration block (Sequence)
-SEQUENCE = 1000
-
-print("=" * 10, "Sender Bitcoin Account")
-
-# Initializing sender Bitcoin wallet
-sender_wallet = Wallet(network=NETWORK)
-# Initializing Bitcoin wallet from address
-sender_wallet.from_address(address=SENDER_ADDRESS)
-# Getting sender wallet information's
-sender_address = sender_wallet.address()
-print("Sender Address:", sender_address)
-sender_hash = sender_wallet.hash()
-print("Sender Hash:", sender_hash)
-sender_p2pkh = sender_wallet.p2pkh()
-print("Sender P2PKH:", sender_p2pkh)
-sender_p2sh = sender_wallet.p2sh()
-print("Sender P2SH:", sender_p2sh)
-sender_balance = sender_wallet.balance()
-print("Sender Balance:", sender_balance)
-
-print("=" * 10, "Recipient Bitcoin Account")
-
-# Initializing recipient Bitcoin wallet
-recipient_wallet = Wallet(network=NETWORK)
-# Initializing Bitcoin wallet from address
-recipient_wallet.from_address(address=RECIPIENT_ADDRESS)
-# Getting recipient wallet information's
-recipient_address = recipient_wallet.address()
-print("Recipient Address:", recipient_address)
-recipient_hash = recipient_wallet.hash()
-print("Recipient Hash:", recipient_hash)
-recipient_p2pkh = recipient_wallet.p2pkh()
-print("Recipient P2PKH:", recipient_p2pkh)
-recipient_p2sh = recipient_wallet.p2sh()
-print("Recipient P2SH:", recipient_p2sh)
-recipient_balance = recipient_wallet.balance()
-print("Recipient Balance:", recipient_balance)
+SEQUENCE: int = 1000
 
 print("=" * 10, "Hash Time Lock Contract (HTLC) between Sender and Recipient")
 
-# Initializing Hash Time Lock Contract (HTLC)
-htlc = HTLC(network=NETWORK)
-# Building new HTLC contract
-htlc.init(
+# Initialize Hash Time Lock Contract (HTLC)
+htlc: HTLC = HTLC(network=NETWORK)
+# Build new HTLC contract
+htlc.build(
     secret_hash=SECRET_HASH,
-    recipient_address=recipient_address,
-    sender_address=sender_address,
+    recipient_address=RECIPIENT_ADDRESS,
+    sender_address=SENDER_ADDRESS,
     sequence=SEQUENCE,
 )
 
-htlc_bytecode = htlc.bytecode()
-print("HTLC Bytecode:", htlc_bytecode)
-htlc_opcode = htlc.opcode()
-print("HTLC OP_Code:", htlc_opcode)
-htlc_hash = htlc.hash()
-print("HTLC Hash:", htlc_hash)
-htlc_address = htlc.address()
-print("HTLC Address:", htlc_address)
+print("HTLC Bytecode:", htlc.bytecode())
+print("HTLC OP_Code:", htlc.opcode())
+print("HTLC Hash:", htlc.hash())
+print("HTLC Address:", htlc.address())
 
-# Getting HTLC balance
-htlc_balance = get_balance(address=htlc_address, network=NETWORK)
-print("HTLC Balance:", htlc_balance)
+# Get HTLC balance
+print("HTLC Balance:", get_balance(
+    address=htlc.address(), network=NETWORK
+))
