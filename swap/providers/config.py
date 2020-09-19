6a38a5cd4da887e109aa1bd6c312cf2911a2856d
meherett <meherett@zoho.com>
2020-09-19 12:01:00 +0300

    Add: Default path and BIP44.

--- swap/providers/config.py
+++ swap/providers/config.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 
-# Bitcoin
 def bitcoin(blockcypher_token=None):
     if blockcypher_token is None:
         blockcypher_token = "c6ef693d3c024088810e6fac2a1494ee"
@@ -22,6 +21,9 @@ def bitcoin(blockcypher_token=None):
                 "token": blockcypher_token
             }
         },
+        "path": "m/44'/0'/0'/0/0",
+        "BIP44": "m/44'/0'/{account}'/{change}/{address}",
+        "symbol": "SATOSHI",
         "timeout": 60,
         "network": "testnet",
         "sequence": 1000,
@@ -31,11 +33,10 @@ def bitcoin(blockcypher_token=None):
     }
 
 
-# Bytom
 def bytom():
     return {
         "mainnet": {
-            "bytom": "http://localhost:9888",
+            "bytom-core": "http://localhost:9888",
             "blockmeta": "https://blockmeta.com/api/v3",
             "blockcenter": {
                 "v2": "https://bcapi.bystack.com/api/v2/btm",
@@ -43,7 +44,7 @@ def bytom():
             },
         },
         "solonet": {
-            "bytom": "http://localhost:9888",
+            "bytom-core": "http://localhost:9888",
             "blockmeta": None,
             "blockcenter": {
                 "v2": None,
@@ -51,13 +52,16 @@ def bytom():
             },
         },
         "testnet": {
-            "bytom": "http://localhost:9888",
+            "bytom-core": "http://localhost:9888",
             "blockmeta": None,
             "blockcenter": {
                 "v2": None,
                 "v3": None
             },
         },
+        "path": "m/44'/153/1/0/1",
+        "BIP44": "m/44/153/{account}/{change}/{address}",
+        "symbol": "NEU",
         "timeout": 60,
         "asset": "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
         "fee": 10_000_000,
