e293414d4e18f860ea6602f5916bf966a16b6570
meherett <meherett@zoho.com>
2020-09-21 00:56:06 +0300

    Upgrade both RPC's.

--- swap/providers/bitcoin/rpc.py
+++ swap/providers/bitcoin/rpc.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python3
 
+from hdwallet.symbols import BTC, BTCTEST
+from btcpy.structs.transaction import MutableTransaction
+from btcpy.setup import setup as stp
+
 import requests
 import json
 
+from ...utils.exceptions import (
+    AddressError, APIError, NetworkError
+)
 from ..config import bitcoin
-from ...utils.exceptions import AddressError, APIError
-from .utils import is_address
+from .utils import (
+    is_network, is_address
+)
+
+# Bitcoin config
+config = bitcoin()
 
 
-# Request headers
-headers = dict()
-headers.setdefault("Content-Type", "application/json")
-
-# Bitcoin configuration
-bitcoin = bitcoin()
-
-
-# Get balance by address
-def get_balance(address, network="testnet", timeout=bitcoin["timeout"]):
+def get_balance(address: str, network: str = config["network"],
+                headers: dict = config["headers"], timeout: int = config["timeout"]) -> int:
     """
     Get Bitcoin balance.
 
@@ -25,26 +28,34 @@ def get_balance(address, network="testnet", timeout=bitcoin["timeout"]):
     :type address: str
     :param network: Bitcoin network, defaults to testnet.
     :type network: str
-    :param timeout: request timeout, default to 60.
+    :param headers: Request headers, default to common headers.
+    :type headers: dict
+    :param timeout: Request timeout, default to 60.
     :type timeout: int
-    :returns: int -- Bitcoin balance.
+    :returns: int -- Bitcoin balance (SATOSHI amount).
 
     >>> from swap.providers.bitcoin.rpc import get_balance
-    >>> get_balance(bitcoin_address, "mainnet")
+    >>> get_balance(address="mkFWGt4hT11XS8dJKzzRFsTrqjjAwZfQAC", network="testnet")
     25800000
     """
 
     if not is_address(address=address, network=network):
-        raise AddressError("invalid %s %s address" % (network, address))
-    url = str(bitcoin[network]["blockcypher"]["url"]) + ("/addrs/%s/balance" % address)
-    return requests.get(url=url, headers=headers, timeout=timeout).json()["balance"]
+        raise AddressError(f"Invalid Bitcoin '{address}' {network} address.")
+    if not is_network(network=network):
+        raise NetworkError(f"Invalid Bitcoin '{network}' network",
+                           "choose only 'mainnet' or 'testnet' networks.")
+    
+    url = f"{config[network]['blockcypher']['url']}/addrs/{address}/balance"
+    response = requests.get(
+        url=url, headers=headers, timeout=timeout
+    ).json()
+    return response["balance"]
 
 
-# Get unspent transaction by address
-def get_unspent_transactions(address, network="testnet",
-                             include_script=True, limit=15, timeout=bitcoin["timeout"]):
+def get_utxos(address: str, network: str = config["network"], include_script: bool = True,
+              limit: int = 15, headers: dict = config["headers"], timeout: int = config["timeout"]) -> list:
     """
-    Get Bitcoin unspent transaction output (UTXO).
+    Get Bitcoin unspent transaction outputs (UTXO's).
 
     :param address: Bitcoin address.
     :type address: str
@@ -54,107 +65,139 @@ def get_unspent_transactions(address, network="testnet",
     :type include_script: bool
     :param limit: Bitcoin utxo's limit, defaults to 15.
     :type limit: int
-    :param timeout: request timeout, default to 60.
+    :param headers: Request headers, default to common headers.
+    :type headers: dict
+    :param timeout: Request timeout, default to 60.
     :type timeout: int
-    :returns: list -- Bitcoin utxo's.
+    :returns: list -- Bitcoin unspent transaction outputs (UTXO's).
 
-    >>> from swap.providers.bitcoin.rpc import get_unspent_transactions
-    >>> get_unspent_transactions(bitcoin_address, "testnet")
+    >>> from swap.providers.bitcoin.rpc import get_utxos
+    >>> get_utxos(address="mkFWGt4hT11XS8dJKzzRFsTrqjjAwZfQAC", network="testnet")
     [...]
     """
 
     if not is_address(address=address, network=network):
-        raise AddressError("invalid %s %s address" % (network, address))
-    _include_script = "true" if include_script else "false"
-    parameter = dict(limit=limit, unspentOnly="true",
-                     includeScript=_include_script, token=bitcoin[network]["blockcypher"]["token"])
-    url = bitcoin[network]["blockcypher"]["url"] + ("/addrs/%s" % address)
-    response = requests.get(url=url, params=parameter, headers=headers, timeout=timeout).json()
+        raise AddressError(f"Invalid Bitcoin '{address}' {network} address.")
+    if not is_network(network=network):
+        raise NetworkError(f"Invalid Bitcoin '{network}' network",
+                           "choose only 'mainnet' or 'testnet' networks.")
+    
+    parameter = dict(
+        limit=limit, unspentOnly="true", 
+        includeScript=("true" if include_script else "false"),
+        token=config[network]["blockcypher"]["token"]
+    )
+    url = f"{config[network]['blockcypher']['url']}/addrs/{address}"
+    response = requests.get(
+        url=url, params=parameter, headers=headers, timeout=timeout
+    ).json()
     return response["txrefs"] if "txrefs" in response else []
 
 
-# Get transaction detail by hash
-def get_transaction_detail(transaction_id, network="testnet", timeout=bitcoin["timeout"]):
+def get_transaction(transaction_id: str, network: str = config["network"],
+                    headers: dict = config["headers"], timeout: int = config["timeout"]) -> dict:
     """
-    Get transaction detail.
+    Get Bitcoin transaction detail.
 
-    :param transaction_id: Bitcoin transaction hash or transaction id.
+    :param transaction_id: Bitcoin transaction id/hash.
     :type transaction_id: str
     :param network: Bitcoin network, defaults to testnet.
     :type network: str
-    :param timeout: request timeout, default to 60.
+    :param headers: Request headers, default to common headers.
+    :type headers: dict
+    :param timeout: Request timeout, default to 60.
     :type timeout: int
     :returns: dict -- Bitcoin transaction detail.
 
-    >>> from swap.providers.bitcoin.rpc import get_transaction_detail
-    >>> get_transaction_detail(transaction_id, "testnet")
+    >>> from swap.providers.bitcoin.rpc import get_transaction
+    >>> get_transaction(transaction_id="4e91bca76db112d3a356c17366df93e364a4922993414225f65390220730d0c1", network="testnet")
     {...}
     """
 
-    parameter = dict(token=bitcoin[network]["blockcypher"]["token"])
-    url = bitcoin[network]["blockcypher"]["url"] + ("/txs/%s" % transaction_id)
-    return requests.get(url=url, params=parameter,
-                        headers=headers, timeout=timeout).json()
+    if not is_network(network=network):
+        raise NetworkError(f"Invalid Bitcoin '{network}' network",
+                           "choose only 'mainnet' or 'testnet' networks.")
+
+    url = f"{config[network]['blockcypher']['url']}/txs/{transaction_id}"
+    parameter = dict(token=config[network]["blockcypher"]["token"])
+    response = requests.get(
+        url=url, params=parameter, headers=headers, timeout=timeout
+    ).json()
+    return response
 
 
-# Getting decode transaction by transaction raw
-def decoded_transaction_raw(transaction_raw, network="testnet", timeout=bitcoin["timeout"]):
+def decode_raw(raw: str, network: str = config["network"], offline: bool = True,
+               headers: dict = config["headers"], timeout: int = config["timeout"]) -> dict:
     """
-    Get decoded transaction raw.
+    Decode Bitcoin raw.
 
-    :param transaction_raw: Bitcoin transaction raw.
-    :type transaction_raw: str
+    :param raw: Bitcoin transaction raw.
+    :type raw: str
     :param network: Bitcoin network, defaults to testnet.
     :type network: str
-    :param timeout: request timeout, default to 60.
+    :param offline: Offline decode, defaults to True.
+    :type offline: bool
+    :param headers: Request headers, default to common headers.
+    :type headers: dict
+    :param timeout: Request timeout, default to 60.
     :type timeout: int
     :returns: dict -- Bitcoin decoded transaction raw.
 
-    >>> from swap.providers.bitcoin.rpc import decoded_transaction_raw
-    >>> decoded_transaction_raw(transaction_raw, "testnet")
+    >>> from swap.providers.bitcoin.rpc import decode_raw
+    >>> decode_raw(raw="...", network="testnet")
     {...}
     """
 
-    if isinstance(transaction_raw, str):
-        parameter = dict(token=bitcoin[network]["blockcypher"]["token"])
-        tx = json.dumps(dict(tx=transaction_raw))
-        return requests.post(url=bitcoin[network]["blockcypher"]["url"] + "/txs/decode",
-                             data=tx, params=parameter, headers=headers, timeout=timeout).json()
-    raise TypeError("transaction raw must be string format!")
+    if not is_network(network=network):
+        raise NetworkError(f"Invalid Bitcoin '{network}' network",
+                           "choose only 'mainnet' or 'testnet' networks.")
+
+    if offline:
+        stp(network, strict=True)
+        tx = MutableTransaction.unhexlify(raw)
+        return tx.json()
+
+    url = f"{config[network]['blockcypher']['url']}/txs/decode"
+    parameter = dict(token=config[network]["blockcypher"]["token"])
+    data = dict(tx=raw)
+    response = requests.post(
+        url=url, data=json.dumps(data), params=parameter, headers=headers, timeout=timeout
+    ).json()
+    return response
 
 
-# Submit payment from blockcypher
-def submit_payment(tx_raw, network="testnet", timeout=bitcoin["timeout"]):
+def submit_raw(raw: str, network: str = config["network"],
+               headers: dict = config["headers"], timeout: int = config["timeout"]) -> str:
     """
-    Submit transaction raw to Bitcoin blockchain.
+    Submit raw into Bitcoin blockchain.
 
-    :param tx_raw: Bitcoin transaction raw.
-    :type tx_raw: str
+    :param raw: Bitcoin transaction raw.
+    :type raw: str
     :param network: Bitcoin network, defaults to testnet.
     :type network: str
-    :param timeout: request timeout, default to 60.
+    :param headers: Request headers, default to common headers.
+    :type headers: dict
+    :param timeout: Request timeout, default to 60.
     :type timeout: int
-    :returns: dict -- Bitcoin decoded transaction raw.
+    :returns: dict -- Bitcoin submitted transaction id/hash.
 
-    >>> from swap.providers.bitcoin.rpc import submit_payment
-    >>> submit_payment(transaction_raw, "testnet")
+    >>> from swap.providers.bitcoin.rpc import submit_raw
+    >>> submit_raw(raw="...", network="testnet")
     {...}
     """
 
-    if isinstance(tx_raw, str):
-        tx = json.dumps(dict(tx_hex=tx_raw))
-        if "mainnet" == network:
-            sochain_network = "BTC"
-        elif "testnet" == network:
-            sochain_network = "BTCTEST"
-        else:
-            raise ValueError("invalid network, only mainnet or testnet")
-        url = bitcoin[network]["sochain"] + f"/send_tx/{sochain_network}"
-        response = requests.post(url=url, data=tx, headers=headers, timeout=timeout)
-        if "status" in response.json() and response.json()["status"] == "fail":
-            raise APIError(response.json()["data"]["tx_hex"])
-        elif "status" in response.json() and response.json()["status"] == "success":
-            return response.json()["data"]
-        else:
-            raise Exception("Unknown Bitcoin submit payment error")
-    raise TypeError("transaction raw must be string format!")
+    if not is_network(network=network):
+        raise NetworkError(f"Invalid Bitcoin '{network}' network",
+                           "choose only 'mainnet' or 'testnet' networks.")
+
+    url = f"{config[network]['sochain']}/send_tx/{BTC if network == 'mainnet' else BTCTEST}"
+    data = dict(tx_hex=raw)
+    response = requests.post(
+        url=url, data=json.dumps(data), headers=headers, timeout=timeout
+    ).json()
+    if "status" in response and response["status"] == "fail":
+        raise APIError(response["data"]["tx_hex"])
+    elif "status" in response and response["status"] == "success":
+        return response["data"]["txid"]
+    else:
+        raise APIError("Unknown Bitcoin submit payment error.")
