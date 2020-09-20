1fa0394816be8896eb4ef36e283b0e01f2cd1d69
meherett <meherett@zoho.com>
2020-09-21 00:56:06 +0300

    Upgrade both RPC's.

--- swap/providers/bytom/rpc.py
+++ swap/providers/bytom/rpc.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-from pybytom.utils import is_network, is_address
-
 import requests
 import json
 
@@ -9,13 +7,16 @@ from ...utils.exceptions import (
     ClientError, APIError, NetworkError, AddressError
 )
 from ..config import bytom
+from .utils import (
+    is_network, is_address
+)
 
 # Bytom config
 config = bytom()
 
 
-def get_balance(address: str, asset: str = config["asset"],
-                network: str = config["network"], timeout: int = config["timeout"]) -> int:
+def get_balance(address: str, asset: str = config["asset"], network: str = config["network"],
+                headers: dict = config["headers"], timeout: int = config["timeout"]) -> int:
     """
     Get Bytom balance.
 
@@ -25,7 +26,9 @@ def get_balance(address: str, asset: str = config["asset"],
     :type asset: str
     :param network: Bytom network, defaults to mainnet.
     :type network: str
-    :param timeout: request timeout, default to 15.
+    :param headers: Request headers, default to common headers.
+    :type headers: dict
+    :param timeout: Request timeout, default to 15.
     :type timeout: int
     :returns: int -- Bytom asset balance (NEU amount).
 
@@ -34,13 +37,16 @@ def get_balance(address: str, asset: str = config["asset"],
     2580000000
     """
 
-    if not is_network(network=network):
-        raise NetworkError(f"Invalid '{network}' network, must be 'mainnet', 'solonet' or 'testnet' networks.")
     if not is_address(address=address, network=network):
-        raise AddressError(f"Invalid '{address}' {network} address/type")
+        raise AddressError(f"Invalid Bytom '{address}' {network} address.")
+    if not is_network(network=network):
+        raise NetworkError(f"Invalid Bytom '{network}' network",
+                           "choose only 'mainnet', 'solonet' or 'testnet' networks.")
 
     url = f"{config[network]['blockmeta']}/address/{address}/asset"
-    response = requests.get(url=url, headers=config["headers"], timeout=timeout)
+    response = requests.get(
+        url=url, headers=headers, timeout=timeout
+    )
     if response.json() is None:
         return 0
     for _asset in response.json():
@@ -49,8 +55,8 @@ def get_balance(address: str, asset: str = config["asset"],
     return 0
 
 
-def build_transaction(address: str, transaction: dict,
-                      network: str = config["network"], timeout: int = config["timeout"]) -> dict:
+def build_transaction(address: str, transaction: dict, network: str = config["network"],
+                      headers: dict = config["headers"], timeout: int = config["timeout"]) -> dict:
     """
     Build Bytom transaction.
 
@@ -60,7 +66,9 @@ def build_transaction(address: str, transaction: dict,
     :type transaction: dict
     :param network: Bytom network, defaults to mainnet.
     :type network: str
-    :param timeout: request timeout, default to 60.
+    :param headers: Request headers, default to common headers.
+    :type headers: dict
+    :param timeout: Request timeout, default to 60.
     :type timeout: int
     :returns: dict -- Bytom built transaction.
 
@@ -69,15 +77,16 @@ def build_transaction(address: str, transaction: dict,
     {...}
     """
 
-    if not is_network(network=network):
-        raise NetworkError(f"Invalid '{network}' network, must be 'mainnet', 'solonet' or 'testnet' networks.")
     if not is_address(address=address, network=network):
-        raise AddressError(f"Invalid '{address}' {network} address/type")
+        raise AddressError(f"Invalid Bytom '{address}' {network} address.")
+    if not is_network(network=network):
+        raise NetworkError(f"Invalid Bytom '{network}' network",
+                           "choose only 'mainnet', 'solonet' or 'testnet' networks.")
 
     url = f"{config[network]['blockcenter']['v3']}/merchant/build-advanced-tx"
     params = dict(address=address)
     response = requests.post(
-        url=url, data=json.dumps(transaction), params=params, headers=config["headers"], timeout=timeout
+        url=url, data=json.dumps(transaction), params=params, headers=headers, timeout=timeout
     )
     if response.status_code == 200 and response.json()["code"] == 300:
         raise APIError(response.json()["msg"], response.json()["code"])
@@ -89,7 +98,7 @@ def build_transaction(address: str, transaction: dict,
 
 
 def get_transaction(transaction_id: str, network: str = config["network"],
-                    timeout: int = config["timeout"]) -> dict:
+                    headers: dict = config["headers"], timeout: int = config["timeout"]) -> dict:
     """
     Get Bytom transaction detail.
 
@@ -97,7 +106,9 @@ def get_transaction(transaction_id: str, network: str = config["network"],
     :type transaction_id: str
     :param network: Bytom network, defaults to mainnet.
     :type network: str
-    :param timeout: request timeout, default to 60.
+    :param headers: Request headers, default to common headers.
+    :type headers: dict
+    :param timeout: Request timeout, default to 60.
     :type timeout: int
     :returns: dict -- Bytom transaction detail.
 
@@ -107,79 +118,88 @@ def get_transaction(transaction_id: str, network: str = config["network"],
     """
 
     if not is_network(network=network):
-        raise NetworkError(f"Invalid '{network}' network/type",
+        raise NetworkError(f"Invalid Bytom '{network}' network",
                            "choose only 'mainnet', 'solonet' or 'testnet' networks.")
+
     url = f"{config[network]['blockcenter']['v2']}/merchant/get-transaction"
     response = requests.post(
-        url=url, data=json.dumps(dict(tx_id=transaction_id)), headers=config["headers"], timeout=timeout
+        url=url, data=json.dumps(dict(tx_id=transaction_id)), headers=headers, timeout=timeout
     )
     if response.status_code == 200 and response.json()["code"] == 300:
         raise APIError(response.json()["msg"], response.json()["code"])
     return response.json()["result"]["data"]
 
 
-def submit_transaction_raw(address: str, transaction_raw: str, signatures: list,
-                           network: str = config["network"], timeout: int = config["timeout"]) -> str:
+def decode_raw(raw: str, network: str = config["network"], 
+               headers: dict = config["headers"], timeout: int = config["timeout"]) -> dict:
     """
-     Submit Bytom transaction raw to blockchain.
+    Decode Bytom raw.
 
-    :param address: Bytom address.
-    :type address: str
-    :param transaction_raw: Bytom transaction raw.
-    :type transaction_raw: str
-    :param signatures: Bytom signed datas.
-    :type signatures: list
-    :param network: Bytom network, defaults to solonet.
+    :param raw: Bytom transaction raw.
+    :type raw: str
+    :param network: Bytom network, defaults to mainnet.
     :type network: str
-    :param timeout: request timeout, default to 60.
-    :type timeout: int
-    :returns: str -- Bytom submitted transaction id/hash.
-
-    >>> from swap.providers.bytom.rpc import submit_transaction_raw
-    >>> submit_transaction_raw(address="bm1q9ndylx02syfwd7npehfxz4lddhzqsve2fu6vc7", transaction_raw="...", signatures=[[...], ...], network="mainnet")
-    "2993414225f65390220730d0c1a356c14e91bca76db112d37366df93e364a492"
-    """
-
-    if not is_network(network=network):
-        raise NetworkError(f"Invalid '{network}' network, must be 'mainnet', 'solonet' or 'testnet' networks.")
-
-    url = f"{config[network]['blockcenter']['v3']}/merchant/submit-payment"
-    data = dict(raw_transaction=transaction_raw, signatures=signatures)
-    params = dict(address=address)
-    response = requests.post(
-        url=url, data=json.dumps(data), params=params, headers=config["headers"], timeout=timeout
-    )
-    if response.json()["code"] != 200:
-        raise APIError(response.json()["msg"], response.json()["code"])
-    return response.json()["data"]["tx_hash"]
-
-
-def decode_transaction_raw(transaction_raw: str, network: str = config["network"],
-                           timeout: int = config["timeout"]) -> dict:
-    """
-    Get decode transaction raw.
-
-    :param transaction_raw: Bytom transaction raw.
-    :type transaction_raw: str
-    :param network: Bytom network, defaults to solonet.
-    :type network: str
-    :param timeout: request timeout, default to 60.
+    :param headers: Request headers, default to common headers.
+    :type headers: dict
+    :param timeout: Request timeout, default to 60.
     :type timeout: int
     :returns: dict -- Bytom decoded transaction raw.
 
-    >>> from swap.providers.bytom.rpc import decode_transaction_raw
-    >>> decode_transaction_raw(transaction_raw="...", network="testnet")
+    >>> from swap.providers.bytom.rpc import decode_raw
+    >>> decode_raw(raw="...", network="testnet")
     {...}
     """
 
     if not is_network(network=network):
-        raise NetworkError(f"Invalid '{network}' network/type",
+        raise NetworkError(f"Invalid Bytom '{network}' network",
                            "choose only 'mainnet', 'solonet' or 'testnet' networks.")
+
     url = f"{config[network]['bytom-core']}/decode-raw-transaction"
-    data = dict(raw_transaction=transaction_raw)
+    data = dict(raw_transaction=raw)
     response = requests.post(
-        url=url, data=json.dumps(data), headers=config["headers"], timeout=timeout
+        url=url, data=json.dumps(data), headers=headers, timeout=timeout
     )
     if response.status_code == 400:
         raise APIError(response.json()["msg"], response.json()["code"])
     return response.json()["data"]
+
+
+def submit_raw(address: str, raw: str, signatures: list, network: str = config["network"],
+               headers: dict = config["headers"], timeout: int = config["timeout"]) -> str:
+    """
+     Submit raw into Bytom blockchain.
+
+    :param address: Bytom address.
+    :type address: str
+    :param raw: Bytom transaction raw.
+    :type raw: str
+    :param signatures: Bytom signed massage datas.
+    :type signatures: list
+    :param network: Bytom network, defaults to mainnet.
+    :type network: str
+    :param headers: Request headers, default to common headers.
+    :type headers: dict
+    :param timeout: Request timeout, default to 60.
+    :type timeout: int
+    :returns: str -- Bytom submitted transaction id/hash.
+
+    >>> from swap.providers.bytom.rpc import submit_raw
+    >>> submit_raw(address="...", raw="...", signatures=[[...], ...], network="...")
+    "2993414225f65390220730d0c1a356c14e91bca76db112d37366df93e364a492"
+    """
+
+    if not is_address(address=address, network=network):
+        raise AddressError(f"Invalid Bytom '{address}' {network} address.")
+    if not is_network(network=network):
+        raise NetworkError(f"Invalid Bytom '{network}' network",
+                           "choose only 'mainnet', 'solonet' or 'testnet' networks.")
+
+    url = f"{config[network]['blockcenter']['v3']}/merchant/submit-payment"
+    data = dict(raw_transaction=raw, signatures=signatures)
+    params = dict(address=address)
+    response = requests.post(
+        url=url, data=json.dumps(data), params=params, headers=headers, timeout=timeout
+    )
+    if response.json()["code"] != 200:
+        raise APIError(response.json()["msg"], response.json()["code"])
+    return response.json()["data"]["tx_hash"]
