e8b06daac3d5fa310c5c9ad67fd80c51dd6ecff8
meherett <meherett@zoho.com>
2020-10-02 12:43:04 +0300

    Update: Both HTLC contact examples.

--- cryptocurrencies/bitcoin/htlc.py
+++ cryptocurrencies/bitcoin/htlc.py
@@ -9,24 +9,25 @@ NETWORK: str = "testnet"
 # Secret password/passphrase hash
 SECRET_HASH: str = sha256("Hello Meheret!")
 # Recipient Bitcoin address
-RECIPIENT_ADDRESS: str = "mwHXvCcug5Rn24c2rpgcRDSo3PyfxZJQQT"
+RECIPIENT_ADDRESS: str = "mgokpSJoX7npmAK1Zj8ze1926CLxYDt1iF"
 # Sender Bitcoin address
-SENDER_ADDRESS: str = "miAcLpYbaqE8KowBu2PwvqXG6y6vpQcfTJ"
+SENDER_ADDRESS: str = "mkFWGt4hT11XS8dJKzzRFsTrqjjAwZfQAC"
 # Expiration block (Sequence)
 SEQUENCE: int = 1000
 
 print("=" * 10, "Hash Time Lock Contract (HTLC) between Sender and Recipient")
 
-# Initialize Hash Time Lock Contract (HTLC)
+# Initialize Bitcoin HTLC
 htlc: HTLC = HTLC(network=NETWORK)
-# Build new HTLC contract
-htlc.build(
+# Build HTLC contract
+htlc.build_htlc(
     secret_hash=SECRET_HASH,
     recipient_address=RECIPIENT_ADDRESS,
     sender_address=SENDER_ADDRESS,
     sequence=SEQUENCE,
 )
 
+# Print all HTLC info's
 print("HTLC Bytecode:", htlc.bytecode())
 print("HTLC OP_Code:", htlc.opcode())
 print("HTLC Hash:", htlc.hash())
