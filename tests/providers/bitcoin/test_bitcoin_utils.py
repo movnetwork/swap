88830633a489d9e8caf8f15a9d9ddc3d4bd0d345
meherett <meherett@zoho.com>
2020-10-07 10:20:03 +0300

    Fix: Exceptions path tests.

--- tests/providers/bitcoin/test_bitcoin_utils.py
+++ tests/providers/bitcoin/test_bitcoin_utils.py
@@ -4,7 +4,7 @@ import pytest
 import json
 import os
 
-from swap.utils.exceptions import APIError
+from swap.exceptions import APIError
 from swap.providers.bitcoin.utils import (
     is_network, is_address, is_transaction_raw,
     decode_transaction_raw, submit_transaction_raw
--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from swap.utils.exceptions import NetworkError, BalanceError, APIError, \
-    AddressError, InvalidURLError, ClientError, NotFoundError
+from swap.exceptions import (
+    NetworkError, BalanceError, APIError, AddressError, InvalidURLError, ClientError, NotFoundError
+)
 
 import pytest
 
